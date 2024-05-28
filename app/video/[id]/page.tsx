e05dbe4066a8838687474467e6dd12d5d023ddd4
M-Hassan-Malik <mhassan.malik.1997@gmail.com>
2024-05-28 20:18:45 +0500

    fix: (video.pages): updated

--- app/video/[id]/page.tsx
+++ app/video/[id]/page.tsx
@@ -1,16 +1,24 @@
 'use client'
 
-import VideoPlayer from '@/app/components/video-player';
 import React from 'react';
+import { usePathname } from "next/navigation";
+import VideoPlayer from '@/app/components/video-player';
 
 const VideoPage: React.FC = () => {
+  const pathname = usePathname();
+  if(!pathname.includes('/video/')){
+    <div>
+    <h1>404 - Page Not Found</h1>
+    <p>The requested page could not be found.</p>
+  </div>
+  } 
 
   return (
     <div>
-      <h1>Video Player</h1>
-      <VideoPlayer id={"4MBVideo"}/>
+      <h1>Video Hub</h1>
+      <VideoPlayer id={pathname.split('/video/')[1]}/>
     </div>
-  );
+  );  
 };
 
 export default VideoPage;
